7cf194a851465fff9b8a0c9562fe0355370b53fa
monkeyswift <131930835+monkeyswift@users.noreply.github.com>
2023-06-09 16:41:41 -0400

    Update polynomials.rs

--- polynomials.rs
+++ polynomials.rs
@@ -1,30 +1,34 @@
+use crate::polynomials::NumType::{Degree, Coefficient};
+
 #[derive(Clone, Debug)]
 pub struct Polynomial {
-    pub coeffs: Vec<u8>
+    pub poly: Vec<(NumType, NumType)>
+}
+#[derive(Clone, Debug, PartialEq)]
+pub enum NumType {
+    Degree (u8),
+    Coefficient (u8),
 }
 
 //remember that addition is just Xor cause of the finite field.
 
 impl Polynomial {
 
-    pub fn evaluate(&self, x: u8) -> u8 {
-        let mut result = 0;
-        for &coeff in self.coeffs.iter().rev() {
-            result = result * x + coeff;
-        }
-        result
+    fn iter(&self) -> std::slice::Iter<(NumType, NumType)> {
+        self.poly.iter()
     }
 
-    fn iter(&self) -> std::slice::Iter<u8> {
-        self.coeffs.iter()
+    fn into_iter(self) -> std::vec::IntoIter<(NumType, NumType)> {
+        self.poly.into_iter()
     }
 
-    fn into_iter(self) -> std::vec::IntoIter<u8> {
-        self.coeffs.into_iter()
-    }
+    fn modulo(self, divisor: Polynomial) -> Polynomial {
+        let res = self.poly[0] / divisor.poly[0];
+        let new_divisor: Vec<u8> = divisor.iter().map(|term| &res * term).collect();
+        let new_dividend = self.iter().zip(new_divisor.iter()).map(|(s_term, dterm)| );
 
-    fn modulo(self, dividend: Polynomial) -> Polynomial {
-        
+        let temp_return = Polynomial {poly: vec![(Degree(1), Coefficient(1)), (Degree(0), Coefficient(1))]};
+        temp_return
     }
 }
 
@@ -36,20 +40,20 @@ impl std::ops::Mul for Polynomial {
     type Output = Polynomial;
     fn mul(self, poly2: Polynomial) -> Polynomial {
         
-        let mut unpadded_poly = self.iter().enumerate().filter(|(_pd, term)| **term == 1).flat_map(|(predegree1, _poly1_term)| {
-            poly2.iter().enumerate().filter(|(pd, term)| **term == 1).map(move|(predegree2, _poly2_term)| -> u8 {
-                let degree1:usize = 8 - predegree1;
-                let degree2:usize = 8 - predegree2;
+        let mut unpadded_poly = self.iter().filter(|(_pd, term)| *term == Coefficient(1)).flat_map(|(predegree1, _poly1_term)| {
+            poly2.iter().filter(|(pd, term)| *term == Coefficient(1)).map(move|(predegree2, _poly2_term)| -> () {
                 
-                (degree1 + degree2) as u8
+                
+                
+                
                 
             })
-        }).collect::<Vec<u8>>();
+        }).collect::<Vec<((NumType, NumType))>>();
 
         //before I clean up and implement addition between the remaining terms I need to decide on how I want to do modulo, as I'm not sure if I will need to pad
         //out the resulting polynomial. 
 
-        Polynomial {coeffs: vec![1]}
+        Polynomial {poly: vec![(Degree(0), Coefficient(0))]}
 
     }
 }
@@ -67,10 +71,23 @@ pub fn polynomial_conversion(columns: Vec<Vec<u8>>) -> Vec<Vec<Polynomial>> {
         column.into_iter().map(|byte| -> Polynomial {
             let mut temp_vec = Vec::new();
             for n in (0..8).rev() {
-               temp_vec.push((byte >> n) & 1);
+                let bit = ((byte >> n) & 1);
+                if bit == 1 {
+                    temp_vec.push((Degree(n), Coefficient(bit)))
+                }
             }
-            Polynomial {coeffs: temp_vec}
+            Polynomial {poly: (temp_vec)}
         }).collect()
     }).collect()
 
 }
+
+
+//Everything below this point is in the scrapyard
+//pub fn evaluate(&self, x: u8) -> u8 {
+    //let mut result = 0;
+    //for &coeff in self.poly.iter().rev() {
+        //result = result * x + coeff;
+    //}
+    //result
+//}
