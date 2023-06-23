75742cbc154c24bf13fa5011f71a084480dc727d
monkeyswift <131930835+monkeyswift@users.noreply.github.com>
2023-06-23 01:51:12 -0400

    Update round_key_generator.rs

--- round_key_generator.rs
+++ round_key_generator.rs
@@ -1,18 +1,15 @@
-//Isolate the last 4 bytes.
-//
-//
-//
-//
-//
 
-pub fn rot_word_transformation(mut key: Vec<String>) -> Vec<String> {
-    let mut getting_worded: Vec<String> = key.drain(12..16).collect();
-
-    let last_element = getting_worded.pop().unwrap();
-
-    getting_worded.insert(0, last_element);
-
-    key.append(&mut getting_worded);
-
+pub fn rot_word_transformation(mut key: Vec<u8>) -> Vec<u8> {
+    let tempval = key.remove(0);
+    key.push(tempval);
     key
+}
+
+pub fn round_constants_generator(mut last_constant: Vec<u8>) -> () {
+    if last_constant[0] < 128 {
+        last_constant[0] = last_constant[0] * 0x02
+    } else {
+        let tempval = (last_constant[0] * 0x02) as u16 ^ 0x11B;
+        last_constant[0] = tempval as u8;
+    }
 }
