c9cc2fe2ec51a462acf5a6094b92c1c0fd3f91df
monkeyswift <131930835+monkeyswift@users.noreply.github.com>
2023-06-27 19:53:37 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
-use encryption::{apply_xor_operation, arrange_into_grid, column_mixing, create_s_box_table, row_shifting};
+use encryption::{xor_operation, column_mixing, create_s_box_table, row_shifting};
 
 use decryption::{reverse_column_mixing, reverse_row_shifting, create_reverse_s_box_table};
 
-use round_key_generator::{rot_word_transformation, sub_word, round_constants_generator};
+use round_key_generator::generate_round_keys;
 
 pub mod encryption;
 
@@ -28,60 +28,29 @@ pub mod polynomials;
 //I am going to iterate 
 
 fn main() {
-    //let limbo_string = String::from(lib::accept_user_input());
 
-    let key = 237282396920938463463374607431768715456 as u128;
+    let key: u128 = 0b00101111001100100010010000001100010110000001101011010100001000000100110011110001111111001010010011110101001001000101011101110011;
 
-    let user_input = String::from("unfathomable huh");
+    let s_box = create_s_box_table(); //probably going to change this in the future. Created an exta hashmap to pass into the key generation for orginazation's sake.
 
-    // since this is just a demonstrative implementation I am going to refrain from constructing an initial vector generator. Maybe I'll change it in the future if I decide to
-    //add more features.
-    let mut dynamic_vector: u128 = 0b00101111001100100010010000001100010110000001101011010100001000000100110011110001111111001010010011110101001001000101011101110011;
-    let mut state = dynamic_vector.to_ne_bytes().to_vec();
-    //xor goes here
+    let round_keys = generate_round_keys(key, s_box);
 
-    let s_box = create_s_box_table();
-    state = state.iter().map(|value| *s_box.get(value).unwrap()).collect();
+    let mut user_input = String::from("unfathomable huh").into_bytes();
+
+    println!("{:?}", user_input);
+
+    let mut state = xor_operation(user_input, key.to_ne_bytes().to_vec());
+
+    let s_box1 = create_s_box_table();
+    state = state.iter().map(|value| *s_box1.get(value).unwrap()).collect();
     state = row_shifting(state);
     state = column_mixing(state);
+    println!("{:?}", state);
+    state = reverse_column_mixing(state);
+    state = reverse_row_shifting(state);
+    let s_box2 = create_reverse_s_box_table();
+    state = state.iter().map(|value| *s_box2.get(value).unwrap()).collect();
+    state = xor_operation(state, key.to_ne_bytes().to_vec());
+    println!("{:?}", state);
 
-    let mut key_vector: Vec<Vec<u8>> = key.to_ne_bytes().chunks(4).map(|x|x.to_vec()).collect();
-
-    for _ in 0..9 {
-        
-        let mut key_vector_iter = key_vector.clone().into_iter();
-
-        let key_vec1 = key_vector_iter.next().unwrap();
-        let key_vec2 = key_vector_iter.next().unwrap();
-        let key_vec3 = key_vector_iter.next().unwrap();
-        let key_vec4 = key_vector_iter.next().unwrap();
-
-        let mut travler = rot_word_transformation(key_vec4.clone());
-
-        travler = travler.iter().map(|value| *s_box.get(value).unwrap()).collect();
-
-        travler = round_constants_generator(travler);
-
-    }
-
-}
-
-fn process_into_blocks(mut user_input: String) -> () {
-    let mut processed_input: Vec<String> = Vec::new();
-    while user_input.len() > 16 {
-        processed_input.push(user_input.drain(0..16).collect());
-    }
-
-    if user_input.len() >= 1 {
-    let mut remains = user_input.clone();
-    for _ in 0..16 - user_input.len() {
-        remains.push('a')
-    }
-    processed_input.push(remains);
-}
-}
-
-#[cfg(test)]
-mod tests {
-    //use super::*;
 }
